b0e5e779684caef76b49ba8090a254f44e0d67a4
madhav1489 <madhavsharma1411@gmail.com>
2026-04-28 06:44:59 +0530

    fix: add correct GitHub and LinkedIn URLs

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -25,7 +25,7 @@ const Contact = () => {
           <div className="contact-box">
             <h4>Social</h4>
             <a
-              href="https://github.com"
+              href="https://github.com/madhav1489"
               target="_blank"
               data-cursor="disable"
               className="contact-social"
@@ -33,7 +33,7 @@ const Contact = () => {
               Github <MdArrowOutward />
             </a>
             <a
-              href="https://www.linkedin.com"
+              href="https://www.linkedin.com/in/madhav-sharma-274709281/"
               target="_blank"
               data-cursor="disable"
               className="contact-social"
--- src/components/SocialIcons.tsx
+++ src/components/SocialIcons.tsx
@@ -59,12 +59,12 @@ const SocialIcons = () => {
     <div className="icons-section">
       <div className="social-icons" data-cursor="icons" id="social">
         <span>
-          <a href="https://github.com" target="_blank">
+          <a href="https://github.com/madhav1489" target="_blank">
             <FaGithub />
           </a>
         </span>
         <span>
-          <a href="https://www.linkedin.com" target="_blank">
+          <a href="https://www.linkedin.com/in/madhav-sharma-274709281/" target="_blank">
             <FaLinkedinIn />
           </a>
         </span>
